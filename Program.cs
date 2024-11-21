48cbe28fde89feebbde475874a47469f29fcb343
Катя <@DESKTOP-8CVJE59>
2024-11-21 18:22:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 12, 8, 23, 81, 5, 19, 77 };
+        Console.WriteLine("Масив: " + string.Join(", ", array));
+        int minIndex = FindMinIndex(array, 0, array.Length - 1);
+        Console.WriteLine("Індекс мінімального елемента: " + minIndex);
+        Console.WriteLine("Мінімальний елемент: " + array[minIndex]);
+        Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+        Console.ReadKey();
+    }
+    static int FindMinIndex(int[] array, int start, int end)
+    {
+        if (start == end)
+            return start;
+        int minIndexInRest = FindMinIndex(array, start + 1, end);
+        if (array[start] < array[minIndexInRest])
+            return start;
+        else
+            return minIndexInRest;
+    }
+}
